6e80ce3ff344cd39838d01d371ed3c07b415b83d
Maga <mcmagakrytoy@gmail.com>
2025-05-25 18:01:23 +0300

    добавление TracksTable

--- main.py
+++ main.py
@@ -90,3 +90,52 @@ class ListenersTable(Table):
         DateBase.commit()
 
 
+class TracksTable(Table):
+
+    @staticmethod
+    def add(values: dict):
+        name = values["NAME"]
+        query = "INSERT INTO `Tracks` (NAME) VALUES (%s)"
+        DateBase.cursor().execute(query, (name,))
+        DateBase.commit()
+    @staticmethod
+    def get_fields_name():
+        return ["NAME","date"]
+
+    @staticmethod
+    def find(values: dict):
+        name = values["NAME"] if "NAME" in values else None
+        date = values["date"] if "date" in values else None
+        (sort_name, asc) = values["sort"] if "sort" in values else (None, None)
+
+        limit = values["limit"] if "limit" in values else None
+        params = []
+
+        if name is not None and date is not None:
+            query = "SELECT `NAME`,`date` FROM `Tracks` WHERE `NAME`=%s AND `date`=%s"
+            params += [name, date]
+        elif name is not None and date is None:
+            query = "SELECT `NAME`,`date` FROM `Tracks` WHERE `NAME`=%s"
+            params += [name]
+        elif name is None and date is not None:
+            query = "SELECT `NAME`,`date` FROM `Tracks` WHERE `date`=%s"
+            params += [date]
+
+        else:
+            query = "SELECT `NAME`,`date` FROM `Tracks`"
+
+        query += f" ORDER BY {sort_name} {asc};"
+        # params += [sort_name]
+        # {'ASC' if asc == 'ASC' else 'DESC'}
+        print(params)
+        print(query)
+        DateBase.cursor().execute(query, params)
+        return DateBase.cursor().fetchall()
+
+    @staticmethod
+    def delete(values: dict):
+        name = values["NAME"]
+        query = "DELETE FROM `Tracks` WHERE `NAME`=%s;"
+        DateBase.cursor().execute(query, (name,))
+        DateBase.commit()
+
