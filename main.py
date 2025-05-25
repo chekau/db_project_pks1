9b4de84d98e7fbade9ff4e051d030554099f428d
Maga <mcmagakrytoy@gmail.com>
2025-05-25 18:00:34 +0300

    добавление ListenersTable

--- main.py
+++ main.py
@@ -39,3 +39,54 @@ class Table(ABC):
     @staticmethod
     def delete(values: dict):
         pass
+
+class ListenersTable(Table):
+
+    @staticmethod
+    def add(values: dict):
+        name = values["NAME"]
+        age = values["age"]
+        query = "INSERT INTO `Listeners` (NAME) VALUES (%s);"
+        DateBase.cursor().execute(query, (name,))
+        DateBase.commit()
+
+    @staticmethod
+    def get_fields_name():
+        return ["NAME","age"]
+    @staticmethod
+    def find(values: dict):
+        name = values["NAME"] if "NAME" in values else None
+        age = values["age"] if "age" in values else None
+        (sort_name, asc) = values["sort"] if "sort" in values else (None,None)
+
+        limit = values["limit"] if "limit" in values else None
+        params = []
+
+        if name is not None and age is not None:
+            query = "SELECT `NAME`,`age` FROM `Listeners` WHERE `NAME`=%s AND `age`=%s"
+            params += [name,age]
+        elif name is not None and age is None:
+            query = "SELECT `NAME`,`age` FROM `Listeners` WHERE `NAME`=%s"
+            params += [name]
+        elif name is None and age is not None:
+            query = "SELECT `NAME`,`age` FROM `Listeners` WHERE `age`=%s"
+            params += [age]
+
+        else:
+            query = "SELECT `NAME`,`age` FROM `Listeners`"
+
+        query += f" ORDER BY {sort_name} {asc};"
+        print(params)
+        print(query)
+        DateBase.cursor().execute(query,params)
+        return DateBase.cursor().fetchall()
+
+
+    @staticmethod
+    def delete(values: dict):
+        name = values["NAME"]
+        query = "DELETE FROM `Listeners` WHERE `NAME`=%s;"
+        DateBase.cursor().execute(query, (name,))
+        DateBase.commit()
+
+
